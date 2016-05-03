66d518cb652bea7a52b07f7ba9e5813491594d0b
Lambo <hnfqzjs@163.com>
2016-05-04 01:18:48 +0800

    Update ViewController.m
    hello帅哥

--- RollBook/ViewController.m
+++ RollBook/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"打印log: hello world！");
+    NSLog(@"打印log: hello 帅哥！");
     
 }
 

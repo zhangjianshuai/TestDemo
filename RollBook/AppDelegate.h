a15e456a9174ddef831c83d6e687ee2d6b6497be
zhangjianshuai <zhangjianshuai>
2016-05-03 17:32:08 +0800

    nihaoya

---
+++ RollBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RollBook
+//
+//  Created by jens on 16/5/3.
+//  Copyright © 2016年 张建帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RollBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RollBook
+//
+//  Created by jens on 16/5/3.
+//  Copyright © 2016年 张建帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
